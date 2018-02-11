236b8e3423a8ce8df8f7a41978b43eed8ea534a2
shaoqin.tsq <shaoqin.tsq@alibaba-inc.com>
2018-02-11 14:51:15 +0800

    init

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into hero(name) values('Black Widow');
+insert into hero(name) values('Superman');
+insert into hero(name) values('Rogue');
+insert into hero(name) values('Batman');
+insert into hero(name) values('Scott');
+insert into hero(name) values('Zuro');
